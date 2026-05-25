1e79953d1b4dab08e891edbd36662c16f709d04a
Wim Audenaert <wim.audenaert@ucamco.com>
2026-05-25 11:04:41 +0200

    Wim - Reconcile search

--- steensterk_custom/__manifest__.py
+++ steensterk_custom/__manifest__.py
@@ -8,12 +8,13 @@
     'author': 'Wim Audenaert',
     'version': '19.0.9.9.9',
     'depends': [
-        'base','project','project_enterprise'
+        'base','project','project_enterprise','account','account_accountant'
     ],
     'data': [
         'security/ir.model.access.csv',
         "views/project_task_view.xml",
         "views/sale_order_view.xml",
+        "views/account_move_line_view.xml",
     ],
     'installable': True,
     'auto_install': True,

dffeb62c4c50fc8ff5b8d294797b3dcb93721b91
Wim Audenaert <wim.audenaert@ucamco.com>
2026-06-02 16:08:13 +0200

    Wim - Ziggu 4

--- steensterk_ziggu/models/__init__.py
+++ steensterk_ziggu/models/__init__.py
@@ -2,6 +2,7 @@
 
 from . import hr_employee
 from . import project_project
+from . import project_task
 from . import project_task_type
 from . import res_config_settings
 from . import res_partner
--- steensterk_ziggu/models/steensterk_decision_type.py
+++ steensterk_ziggu/models/steensterk_decision_type.py
@@ -9,7 +9,9 @@ class steensterk_decision_type(models.Model):
     _order = "name"
 
     name = fields.Char("Naam", required=True, index=True)
+    weeks_after_start = fields.Integer('Weken na Start Project', required=True)
 
     ziggu_decision_type_category_id = fields.Many2one("ziggu.decision.type.category", "Decision Type Category", index=True)
 
     active = fields.Boolean("Active", default=True)
+    
